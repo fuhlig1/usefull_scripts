4e3a4f207376cbdd357a49a509ce21a7d361e3d9
Florian Uhlig <f.uhlig@gsi.de>
2014-05-19 16:14:45 +0200

    Restructuring and cleaning of the script.

--- build_clang.sh
+++ build_clang.sh
@@ -4,126 +4,234 @@
 ####
 
 # Variables which have to be adjusted to the needs of the system
+# At least define the installation dir
+# TODO: Change interface to pass the the temporary and the install dir
+
 version=34
 version_full=3.4
-source_dir=/tmp/build_llvm/$version_full
-build_dir=/tmp/build_llvm/build/$version_full
-build_dir_stage2=/tmp/build_llvm/build/${version_full}_stage2
-tmpInstDir=/tmp/compiler_tmp/llvm/$version_full
-InstDir=/Users/uhlig/compiler/llvm/$version_full
 
-# Extract the directory where the script is loacted 
-script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+tmpDir=/tmp/build_llvm_2/
+InstDir=/Users/uhlig/compiler/llvm
 
-# check if the used compiler used if either gcc or clang
-# and if the compiler is really available
-compiler=$(basename "$CC")
-if [ -z "$CC" ];
-then
-  compiler=gcc
-fi
-if [ "$compiler" != "clang" -a "$compiler" != "gcc" ];
-then
-  echo "The script only works with clang or gcc."
-  exit 1
-else
-  answer=$(which $compiler)
-  if [ "$answer" != "" ];
-  then
-    no_program=$(which $compiler | grep -c '^no' )
-    no_program1=$(which $compiler | grep -c "^no $compiler")
-    if [ "$no_program" != "0" -o "$no_program1" != "0" ];
-    then
-      answer=""  
-    fi
-  fi
-  if [ "$answer" == "" ];
-  then
-    echo "Could not find compiler $compiler."
-    exit 1
-  fi
-fi
+# unset environment variables
+unset CFLAGS
+unset CXXFLAGS
+unset CPPFLAGS
+unset LDFLAGS
+unset LIBRARY_PATH
+unset LD_LIBRARY_PATH
+unset DYLD_LIBRARY_PATH
 
-# check compiler version. If the compiler is too old e.g. gcc 4.4 one cannot compile
-# newer versions of llvm/clang. One has to compile first clang 3.2 and use this version
-# to compile the final version of llvm/clang.
-if [ "$compiler" = "clang" ];
-then
-  compiler_version=$(clang -v 2>&1 | sed -n 1p | cut -d' ' -f 3)
-  if [ "$compiler_version" = "version" ]; # we are on mac
-  then
-    compiler_version=$(clang -v 2>&1 | sed -n 1p | cut -d' ' -f 9 | cut -c1-3)
-  fi
-  major=$(echo $compiler_version | cut -d. -f1 ) 
-  minor=$(echo $compiler_version | cut -d. -f2) 
-else
-  compiler_version=$(gcc -dumpversion)
-  major=$(echo $compiler_version | cut -d. -f1 ) 
-  minor=$(echo $compiler_version | cut -d. -f2) 
-fi
-bootstrap=no
-if [ "$compiler" = "gcc" ];
-then
-  if [ $major -lt 4 ];
-  then
-     bootstrap=yes
-  elif [ $minor -lt 5 ];
-  then
-    bootstrap=yes   
-  fi  
-elif [ "$compiler" = "clang" ];
-then
-  if [ $major -eq 3 -a $minor -ge 2 ];
-  then
-    bootstrap=no
-  else
-    bootstrap=yes
-  fi
-fi
+main() {
+  # Extract the directory where the script is loacted 
+  script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+  check_architecture
+  check_compiler
+  if [ "$bootstrap" = "yes" ];
+  then 
+    version_final=$version
+    version_full_final=$version_full
+    bootstrap_settings
+    download_llvm_core
+    build_pre_stage1
+    cc=$tmpInstDir/bin/clang
+    cxx=$tmpInstDir/bin/clang
+    build_cxxabi 
+    version=$version_final
+    version_full=$version_full_final
+  fi 
+  final_settings
+  download_llvm_core
+  download_llvm_addons
+  build_stage1 
+#  build_stage2
+  exit
+
+}
+
+usage() {
+  echo ""
+  echo ""
+  echo "The script will install clang/llvm ${version_full} from sources."
+  echo ""
+  echo "It will check the existing compiler infrastructure. Supported" 
+  echo "compilers are clang and gcc, where gcc is the default. If you want" 
+  echo "to use clang you have to the environment variable CC which has to" 
+  echo "point to to the clang executable."
+  echo ""
+  echo "In case the compiler is to old to compile the required"
+  echo "clang/llvm version the script will install first an older clang/llvm"
+  echo "version to compile the final one."
+  echo ""
+  echo "Beside clang/llvm the script will install also some other usefull"
+  echo "tools for software development."
+  echo ""
+  echo "include-what-you-use: https://code.google.com/p/include-what-you-use/"
+  echo "   tool to check the #include statements"
+  echo ""
+  echo "oclint: http://oclint.org/"
+  echo "   static code analysis tool" 
+  echo ""
+  echo ""
+  exit 0
+}
+
+final_settings() {
+  source_dir=$tmpDir/$version_full
+  build_dir=$tmpDir/build/$version_full
+  build_dir_stage2=$tmpDir/build/${version_full}_stage2
+  tmpInstDir=$tmpDir/compiler_tmp/llvm/$version_full
+  InstDir=$InstDir/$version_full
+}
 
 # Define directories for bootstrap installation
-if [ "$bootstrap" = "yes" ];
-then 
-  echo "Your compiler is to old to compile version $version_full of Clang directly."
-  version_tmp=32
-  version_tmp_full=3.2
-  source_dir=${source_dir/%$version_full/$version_tmp_full}
-  build_dir=${build_dir/%$version_full/$version_tmp_full}
-  tmpInstDir=${tmpInstDir/%$version_full/$version_tmp_full}
-  InstDir=${InstDir/%$version_full/$version_tmp_full}
-  echo "So we will build first Clang $version_tmp_full and use this version to"
-  echo "compile the final Clang version $version_full."
+bootstrap_settings() {
+  local version_tmp=32
+  local version_tmp_full=3.2
+  source_dir=$tmpDir/$version_tmp_full
+  build_dir=$tmpDir/build/$version_tmp_full
+  tmpInstDir=$tmpDir/compiler_tmp/llvm/$version_tmp_full
+  InstDir=$tmpInstDir
+  cxxabi_include_path=$InstDir/include/cxxabi
+  cxxabi_lib_path=$InstDir/lib
+  echo "So we will first build clang/llvm $version_tmp_full and use this version to compile the final clang/llvm version $version_full."
   version=32
   version_full=3.2
-  sleep 5
-fi 
+  sleep 2
+}
 
-mkdir -p $source_dir/llvm
-cd $source_dir/llvm
-
-if [ ! -d $version ]; then
-  svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$version/final $version
-fi
-
-cd $source_dir/llvm/$version/tools
-if [ ! -d clang ]; then
-  svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$version/final clang
-fi
-
-cd $source_dir/llvm/$version/projects
-if [ "$bootstrap" = "yes" ];
-then
-  if [ ! -d libcxx ]; then
-    svn co http://llvm.org/svn/llvm-project/libcxx/branches/release_$version libcxx
+# check if the used compiler used if either gcc or clang,
+# if the compiler is really available, and if the installed
+# compiler version is not to old to compile clang/llvm
+# At least clang 3.2 or gcc 4.7 is needed to 
+check_compiler() {
+  
+  local compiler=$(basename "$CC")
+  if [ -z "$CC" ];
+  then
+    compiler=gcc
   fi
-else  
-  if [ ! -d libcxx ]; then
-    svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$version/final libcxx
+  if [ "$compiler" != "clang" -a "$compiler" != "gcc" ];
+  then
+    echo "The script only works with clang or gcc."
+    usage
+  else
+    local answer
+    local no_program
+    local no_program1
+    answer=$(which $compiler)
+    if [ "$answer" != "" ];
+    then
+      no_program=$(which $compiler | grep -c '^no' )
+      no_program1=$(which $compiler | grep -c "^no $compiler")
+      if [ "$no_program" != "0" -o "$no_program1" != "0" ];
+      then
+        answer=""  
+      fi
+    fi
+    if [ "$answer" == "" ];
+    then
+      echo "Could not find compiler $compiler."
+      usage
+    fi
   fi
-fi 
 
-if [ "$bootstrap" = "no" ];
-then 
+  local compiler_version
+  local minor
+  local major
+  if [ "$compiler" = "clang" ];
+  then
+    compiler_version=$(clang -v 2>&1 | sed -n 1p | cut -d' ' -f 3)
+    if [ "$compiler_version" = "version" ]; # we are on mac
+    then
+      compiler_version=$(clang -v 2>&1 | sed -n 1p | cut -d' ' -f 9 | cut -c1-3)
+    fi
+    major=$(echo $compiler_version | cut -d. -f1 ) 
+    minor=$(echo $compiler_version | cut -d. -f2) 
+  else
+    compiler_version=$(gcc -dumpversion)
+    major=$(echo $compiler_version | cut -d. -f1 ) 
+    minor=$(echo $compiler_version | cut -d. -f2) 
+  fi
+  bootstrap=no
+  if [ "$compiler" = "gcc" ];
+  then
+    if [ $major -lt 4 ];
+    then
+       bootstrap=yes
+    elif [ $minor -lt 7 ];
+    then
+      bootstrap=yes   
+    fi  
+  elif [ "$compiler" = "clang" ];
+  then
+    if [ $major -eq 3 -a $minor -ge 2 ];
+    then
+      bootstrap=no
+    else
+      bootstrap=yes
+    fi
+  fi
+  if [ "$bootstrap" = "yes" ];
+  then 
+    echo "Your compiler $compiler $major.$minor is to old to compile clang/llvm $version_full directly."
+  fi
+
+}
+
+# test for architecture
+# get the number of processors
+# and information about the host
+# set some architecture specific variables
+check_architecture() {
+  arch=$(uname -s | tr '[A-Z]' '[a-z]')
+  if [ "$arch" = "linux" ];
+  then
+    ncpu=$(cat /proc/cpuinfo | grep processor | wc -l)
+    triple=-linux-
+    ext=so
+    cxxabi_checkfile=libc++abi.$ext.1.0
+  elif [ "$arch" = "darwin" ];
+  then
+    mac_version=$(sw_vers -productVersion | cut -d . -f 1-2)
+    ncpu=$(sysctl -n hw.ncpu)
+    triple=-apple-
+    ext=dylib
+    cxxabi_checkfile=libc++abi.$ext
+  else
+    echo "The script supports only linux and MacOSX (darwin)."
+  fi
+}
+
+download_llvm_core() {
+
+  mkdir -p $source_dir/llvm
+  cd $source_dir/llvm
+
+  if [ ! -d $version ]; then
+    svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$version/final $version
+  fi
+
+  cd $source_dir/llvm/$version/tools
+  if [ ! -d clang ]; then
+    svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$version/final clang
+  fi
+
+  cd $source_dir/llvm/$version/projects
+  if [ "$bootstrap" = "yes" ];
+  then
+    if [ ! -d libcxx ]; then
+      svn co http://llvm.org/svn/llvm-project/libcxx/branches/release_$version libcxx
+    fi
+  else  
+    if [ ! -d libcxx ]; then
+      svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$version/final libcxx
+    fi
+  fi 
+}
+
+download_llvm_addons() {
   cd $source_dir/llvm/$version/tools/clang/tools/
   if [ ! -d  extra ]; then
     svn co http://llvm.org/svn/llvm-project/clang-tools-extra/branches/release_$version extra
@@ -131,62 +239,121 @@ then
 
   if [ ! -d  include-what-you-use ]; then
     svn co http://include-what-you-use.googlecode.com/svn/branches/clang_$version_full include-what-you-use
-
-  patch -p0 << EOF
---- CMakeLists.txt~	2014-01-20 16:31:17.000000000 +0100
-+++ CMakeLists.txt	2014-01-20 16:34:08.000000000 +0100
-@@ -20,3 +20,4 @@
- # to keep the primary Clang repository small and focused.
- # It also may be included by LLVM_EXTERNAL_CLANG_TOOLS_EXTRA_SOURCE_DIR.
- add_llvm_external_project(clang-tools-extra extra)
-+add_subdirectory(include-what-you-use)
-EOF
   fi
 
   cd $source_dir/llvm/$version/projects
   if [ ! -d compiler-rt ]; then
     svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_$version/final compiler-rt
-  patch -p0 << EOF
---- compiler-rt/CMakeLists.txt_orig 2014-05-13 10:23:48.000000000 +0200
-+++ compiler-rt/CMakeLists.txt 2014-05-13 10:24:11.000000000 +0200
-@@ -188,9 +188,9 @@
-     OUTPUT_STRIP_TRAILING_WHITESPACE
-   )
-   set(SANITIZER_COMMON_SUPPORTED_DARWIN_OS osx)
--  if (IOSSIM_SDK_DIR)
--    list(APPEND SANITIZER_COMMON_SUPPORTED_DARWIN_OS iossim)
--  endif()
-+#  if (IOSSIM_SDK_DIR)
-+#    list(APPEND SANITIZER_COMMON_SUPPORTED_DARWIN_OS iossim)
-+#  endif()
- 
-   if(COMPILER_RT_USES_LIBCXX)
-     set(SANITIZER_MIN_OSX_VERSION 10.7)
-EOF
-  fi
+  fi  
+}
+
+build_llvm() {
+  set -xv
+  CXXFLAGS=$cxxflags LDFLAGS=$ldflags \
+  cmake $source_dir/llvm/$version \
+    -DCMAKE_INSTALL_PREFIX=$tmpInstDir $cmakeflags
+ set +xv
+
+  make -j$ncpu 
+  make install
   
-fi
+  # create symbolic links for cc and c++ 
+  cd $tmpInstDir/bin
+  ln -s clang cc
+  ln -s clang++ c++
+}
 
-# test for architecture
-arch=$(uname -s | tr '[A-Z]' '[a-z]')
-# get the number of processors
-# and information about the host
-if [ "$arch" = "linux" ];
-then
-  ncpu=$(cat /proc/cpuinfo | grep processor | wc -l)
-  triple=-linux-
-  ext=so
-  cxxabi_checkfile=libc++abi.$ext.1.0
-elif [ "$arch" = "darwin" ];
-then
-  mac_version=$(sw_vers -productVersion | cut -d . -f 1-2)
-  ncpu=$(sysctl -n hw.ncpu)
-  triple=-apple-
-  ext=dylib
-  cxxabi_checkfile=libc++abi.$ext
-fi
-    
+build_pre_stage1() {
+ 
+  if [ ! -f $tmpInstDir/bin/clang ]; then
+    mkdir -p $build_dir
+    cd $build_dir
+    if [ "$mac_version" = "10.6" ];
+    then
+      cxxflags="-U__STRICT_ANSI__"  
+      ldflags=""
+      cmakeflags=""
+    fi
+    build_llvm
+  fi
+}
 
+# compile libcxxabi which is needed for a standalone version of libc++
+# information taken from
+# http://dragoonsheir.wordpress.com/2013/03/16/wayland-and-c11-programming-part-1-of-n/
+build_cxxabi() {
+  if [ ! -f $InstDir/lib/$cxxabi_checkfile ]; then
+    cd $source_dir
+    mkdir -p libc++
+    cd libc++
+
+    svn co -r 200202 http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+    cd libcxxabi/lib
+
+    if [ "$arch" = "darwin" ];
+    then
+      sed 's#-install_name /usr/lib/libc++abi.dylib#-install_name $InstDir/lib/libc++abi.dylib#g' -i' ' buildit  
+    fi    
+
+    ls -la $source_dir/llvm/$version/projects/libcxx/include
+    InstDir=$InstDir \
+      CC=$cc CXX=$cxx \
+      CPATH=$source_dir/llvm/$version/projects/libcxx/include \
+      Library_PATH=$tmpInstDir/lib \
+      TRIPLE=$triple ./buildit
+  
+    if [ ! -f $cxxabi_checkfile ]; then
+      exit
+    fi  
+
+    mkdir -p $InstDir/include/cxxabi
+    cp -r ../include/* $InstDir/include/cxxabi
+ 
+    mkdir -p $InstDir/lib
+    cp  $cxxabi_checkfile $InstDir/lib
+    if [ "$arch" = "linux" ];
+    then
+      cd $InstDir/lib
+      ln -s libc++abi.$ext.1.0 libc++abi.$ext.1
+      ln -s libc++abi.$ext.1 libc++abi.$ext
+    fi
+  fi
+}
+
+build_stage1() {
+  if [ ! -f $tmpInstDir/bin/clang ]; then
+    cd $source_dir/llvm/$version/
+    patch -p0 < $script_dir/llvm_core.patch
+    patch -p0 < $script_dir/llvm_addons.patch
+    patch -p0 < $script_dir/llvm_libcxx_macosx.patch
+    if [ "$mac_version" = "10.6" ];
+    then
+      patch -p0 < $script_dir/llvm_libcxx_macosx_10_6.patch
+      cxxflags="-U__STRICT_ANSI__"
+      ldflags="-L$cxxabi_lib_path"
+#      cxxflags="-U__STRICT_ANSI__ -I$cxxabi_include_path"  
+#      ldflags="-L$cxxabi_lib_path -lc++abi"
+      cmakeflags="-DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_LIBCXXABI_INCLUDE_PATHS=$cxxabi_include_path -DLIBCXX_LIBCXXABI_LIBRARY_PATH=$cxxabi_lib_path -DLIBCXX_INSTALL_PATH=$tmpInstDir/lib"   
+    fi
+    mkdir -p $build_dir
+    cd $build_dir
+    export CC=$cc 
+    export CXX=$cxx 
+    echo "CC: $CC"
+    echo "CXX: $CXX"
+    build_llvm
+  fi  
+}
+
+main "$@"
+
+echo "Should not come here"
+exit
+
+
+
+
+   
 if [ ! -f $tmpInstDir/bin/clang ]; then
   mkdir -p $build_dir
   cd $build_dir
@@ -228,64 +395,12 @@ if [ ! -f $tmpInstDir/bin/clang ]; then
   ln -s clang++ c++
 fi
 
-if [ "$bootstrap" = "yes" ];
-then 
-  export PATH=$tmpInstDir/bin:$PATH
-  export CC=$tmpInstDir/bin/clang
-  export CXX=$tmpInstDir/bin/clang++
-fi
-
-# compile libcxxabi which is needed for a standalone version of libc++
-# information taken from
-# http://dragoonsheir.wordpress.com/2013/03/16/wayland-and-c11-programming-part-1-of-n/
-
-if [ "$bootstrap" = "yes" ];
-then 
-  InstDirTmp=$tmpInstDir
-else
-  InstDirTmp=$InstDir
-fi
-
-if [ ! -f $InstDirTmp/lib/$cxxabi_checkfile ]; then
-  cd $source_dir
-  mkdir -p libc++
-  cd libc++
-#  svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-#  svn co http://llvm.org/svn/llvm-project/libcxxabi/branches/release_32 libcxxabi
-  svn co -r 200202 http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-  cd libcxxabi/lib
-
-  if [ "$arch" = "darwin" ];
-  then
-    sed 's#-install_name /usr/lib/libc++abi.dylib#-install_name $InstDirTmp/lib/libc++abi.dylib#g' -i' ' buildit  
-  fi    
-        
-  InstDirTmp=$InstDirTmp CC=$tmpInstDir/bin/clang CXX=$tmpInstDir/bin/clang++ \
-  CPATH=$source_dir/llvm/$version/projects/libcxx/include LIBRARY_PATH=$tmpInstDir/lib \
-  TRIPLE=$triple ./buildit
-  
-  if [ ! -f $cxxabi_checkfile ]; then
-    exit
-  fi  
-
-  mkdir -p $InstDirTmp/include/cxxabi
-  cp -r ../include/* $InstDirTmp/include/cxxabi
- 
-  mkdir -p $InstDirTmp/lib
-  cp  $cxxabi_checkfile $InstDirTmp/lib
-  if [ "$arch" = "linux" ];
-  then
-    cd $InstDirTmp/lib
-    ln -s libc++abi.$ext.1.0 libc++abi.$ext.1
-    ln -s libc++abi.$ext.1 libc++abi.$ext
-  fi
-fi
 
 if [ "$bootstrap" = "yes" ];
 then 
   echo "Now we will build the final version of clang"
-  export cxxabi_include_path=$InstDirTmp/include/cxxabi
-  export cxxabi_lib_path=$InstDirTmp/lib
+  export cxxabi_include_path=$InstDir/include/cxxabi
+  export cxxabi_lib_path=$InstDir/lib
   $script_dir/build_clang.sh  
   exit
 fi
